f5f12e2d0ec3fa6dbe89095281bb809d8c8e9fcb
papeiron <151983680+papeiron@users.noreply.github.com>
2023-12-31 22:53:10 +0300

    fix

--- src/redux/slice/categoryImagesSlice.ts
+++ src/redux/slice/categoryImagesSlice.ts
@@ -3,10 +3,10 @@ import { createSlice } from '@reduxjs/toolkit';
 export type CategoryImages = [string, string];
 
 const initialState: CategoryImages[] = [
-  ['Living room', '/src/assets/img/category-images/category-livingroom.png'],
-  ['Kitchen', '/src/assets/img/category-images/category-kitchen.png'],
-  ['Bedroom', '/src/assets/img/category-images/category-bedroom.png'],
-  ['Bathroom', '/src/assets/img/category-images/category-bathroom.png']
+  ['Living room', '/public/category-images/category-livingroom.png'],
+  ['Kitchen', '/public/category-images/category-kitchen.png'],
+  ['Bedroom', '/public/category-images/category-bedroom.png'],
+  ['Bathroom', '/public/category-images/category-bathroom.png']
 ];
 
 export const categoryImagesSlice = createSlice({
--- src/redux/slice/sliderImagesSlice.ts
+++ src/redux/slice/sliderImagesSlice.ts
@@ -1,9 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState: string[] = [
-  '/src/assets/img/hero-images/livingroom-2.png',
-  '/src/assets/img/hero-images/bathroom-1.png',
-  '/src/assets/img/hero-images/kitchen-1.png'
+  '/public/hero-images/livingroom-2.png',
+  '/public/hero-images/bathroom-1.png',
+  '/public/hero-images/kitchen-1.png'
 ];
 
 export const sliderImagesSlice = createSlice({
