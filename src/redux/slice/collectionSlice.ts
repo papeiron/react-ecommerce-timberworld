42713889507f72c62e7f9183284cec1d459b456b
papeiron <151983680+papeiron@users.noreply.github.com>
2023-12-31 23:17:57 +0300

    fix

--- src/redux/slice/collectionSlice.ts
+++ src/redux/slice/collectionSlice.ts
@@ -10,12 +10,18 @@ const initialState: Collections[] = [
   {
     title: 'Wooden World',
     text: 'Towering trees and lush forests create a serene realm, showcasing natures wooden marvels in harmonious coexistence with humanity.',
-    img: ['/public/collections-images/wooden-1.png', '/public/collections-images/wooden-2.png']
+    img: [
+      '/src/assets/img/collections-images/wooden-1.png',
+      '/src/assets/img/collections-images/wooden-2.png'
+    ]
   },
   {
     title: 'Nordic Simplicity',
     text: 'Discover the essence of Scandinavian design with our Nordic Charm collection. Celebrating simplicity, functionality, and understated beauty, these pieces transform any space into a serene and stylish haven.',
-    img: ['/public/collections-images/nordic-1.png', '/public/collections-images/nordic-2.png']
+    img: [
+      '/src/assets/img/collections-images/nordic-1.png',
+      '/src/assets/img/collections-images/nordic-2.png'
+    ]
   }
 ];
 
